2220c72d60d7f27e3b758ada32e3458d988340d1
Akash Mallik <amallik@idare.io>
2020-10-20 01:32:31 +0600

    Notification consumer added

--- src/notifier/consumers.py
+++ src/notifier/consumers.py
@@ -26,3 +26,16 @@ class TickTockConsumer(AsyncJsonWebsocketConsumer):
             await self.send_json("tick")
             await asyncio.sleep(1)
             await self.send_json("....tock")
+
+
+class NotificationConsumer(AsyncJsonWebsocketConsumer):
+
+    async def connect(self):
+        await self.accept()
+        await self.channel_layer.group_add("gossip", self.channel_name)
+
+    async def disconnect(self, code):
+        await self.channel_layer.group_discard("gossip", self.channel_name)
+
+    async def user_gossip(self, event):
+        await self.send_json(event)
--- src/notifier/routing.py
+++ src/notifier/routing.py
@@ -1,11 +1,12 @@
 from channels.routing import URLRouter
 from django.urls import path
 
-from .consumers import EchoConsumer, TickTockConsumer
+from .consumers import EchoConsumer, TickTockConsumer, NotificationConsumer
 
 websocket_urlpatterns = [
     path('ws/', URLRouter([
         path('', EchoConsumer),
-        path('ticktock/', TickTockConsumer)
+        path('ticktock/', TickTockConsumer),
     ])),
+    path('notifications/', NotificationConsumer)
 ]
